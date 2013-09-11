e5283e4a3782069e3a24fca18ea0e3436ea51681
Shashank Rao <rshashank192@gmail.com>
2013-09-11 19:50:40 +0530

    Added script for word validity

---
+++ scripts/validword.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+# Quick spell checker module for word validity 
+# to be used in cows-and-bulls
+# Author: Shashank S Rao
+
+def check(guess):
+  try:
+    f = open("4letterwords.txt","r")
+  except:
+    import sys
+    print "Unable to find dictionary"
+    sys.exit()
+  words = f.read().split()
+  if guess.upper() in words:
+    #checking for duplicate letters in word
+    if len(set(guess))==4:
+      return True
+    else:
+      return False
+  else:
+    return False
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv)<2:
+      print "Usage: python checker.py <guess>"
+      sys.exit()
+    if check(sys.argv[1]) == True:
+      print "valid"
+    else:
+      print "invalid"
