f2b2f1ae55d61740243bba38accb6b325768b1e5
Shashank Rao <rshashank192@gmail.com>
2013-09-11 22:56:56 +0530

    Added master script

---
+++ scripts/main.py
@@ -0,0 +1,17 @@
+from randomwordgenerator import *
+from checker import *
+from validword import *
+
+def main(word,guess):
+	if check(guess):
+		print "valid"
+		print checker(word,guess)
+	else:
+		print "invalid"
+
+
+if __name__ == '__main__':
+	import sys
+	if len(sys.argv)<3:
+		print "python main.py word guess"
+	main(sys.argv[1],sys.argv[2])
