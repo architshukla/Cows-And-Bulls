41bbe39282842af79b3daf6358f208ad9b82ea89
Archit Shukla <architshukla2@gmail.com>
2013-09-16 00:49:01 +0530

    Added recordFeedback.php to log ratings/feedback

---
+++ recordFeedback.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();
+	$ratings = $_POST['ratings'];
+	$feedback = $_POST['feedback'];
+	if(empty($_SESSION['username']))
+	{
+		echo "<b>You are not logged in. </b> Please log in and try again.";
+		return;
+	}
+	$date = getdate();
+	$filename = $date['mday']."_".$date['mon']."_".$date['year'];
+
+	$handle = fopen("feedback/$filename","a");
+	fwrite($handle, $_SESSION['username']."|".$ratings."|".$feedback."\r\n");
+	fclose($handle);
+
+	echo "success";
+?>
