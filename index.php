63d9edab6c1719abc0d6dbb090b38401e7d11e99
Archit Shukla <architshukla2@gmail.com>
2013-09-17 01:06:45 +0530

    Added text in About, changed delimiter for feedback

--- index.php
+++ index.php
@@ -143,8 +143,25 @@
      <h4 class="modal-title">About - Cows and Bulls</h4>
    </div>
    <div class="modal-body">
-    <p> Cows and Bulls </p>
-    <!-- TODO -->
+    <div class='well'>
+    <p><strong>Cows and Bulls</strong> is a game where a player guesses a word (usually, a 4-letter word without repeating letters) given by another player. Each word is qualified as follows:</p>
+    <ol>
+      <li> A <strong> "Bull" </strong> is a letter in the guessed word which appears in the same positon as it appears in the original word.</li>
+      <li> A <strong> "Cow" </strong> is a letter in the guessed word which appears in a different posiiton as it appeats in the original word.</li>
+    </ol>
+    <br><p>Proper nouns, abbreviations and acronyms are not allowed.</p>
+    <p>The game continues until the player guesses the word correctly, that is, gets "4 Bulls" on a word. Then the opponent gets to play.</p>
+    </div>
+
+    <h3 class='text-info'>Examples</h3>
+    <div class='well'>
+    <ol>
+      <li> If the original word is <strong>"GAME"</strong>, and the player guesses the word to be <strong>"FAKE"</strong>, the word is said to have <strong>"2 Bulls"</strong> as the letters A and E appear in GAME and FAKE in the same places</li>
+      <li> If the original word is <strong>"GAME"</strong>, and the player guesses the word to be <strong>"HOAX"</strong>, the word is said to have <strong>"1 Cow"</strong> as the letter A appears in both GAME and HOAX but in different places.</li>
+      <li> If the original word is <strong>"GAME"</strong>, and the player guesses the word to be <strong>"EATS"</strong>, the word is said to have <strong>"1 Bull and 1 Cow"</strong> where E is the "Cow" and A is the "Bull".</li>
+    </ol>
+</div>
+
   </div>
   <div class="modal-footer">
     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
